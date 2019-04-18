f907c4cf0a22caf57b7800dfc0715141e445f959
Filipe Santos <filipesantos@live.ca>
2019-04-18 11:57:24 -0400

    Update caption picture carousel

--- index.php
+++ index.php
@@ -67,52 +67,64 @@
     <div class="carousel-inner" style="height: 100%">
       <div class="carousel-item active" data-interval="5000" style="height: 100%">
         <img src="assets/images/island.jpg" class="w-100" alt="La plage" style="height: 100%">
+        <div class="my-carousel-caption d-none d-md-block">
+          <h1>First slide label</h1>
+          <h3>Nulla vitae elit libero, a pharetra augue mollis interdum.</h3>
+        </div>
       </div>
       <div class="carousel-item" data-interval="5000" style="height: 100%">
-        <img src="assets/images/mountain.jpg" class="w-100" alt="..." style="height: 100%">
+        <img src="assets/images/mountain.jpg" class="w-100" alt="..." style="height: 100%;">
+        <div class="my-carousel-caption d-none d-md-block">
+          <h1>First slide label</h1>
+          <h3>Nulla vitae elit libero, a pharetra augue mollis interdum.</h3>
+        </div>
       </div>
       <div class="carousel-item" data-interval="5000" style="height: 100%">
         <img src="assets/images/village.jpg" class="w-100" alt="..." style="height: 100%">
+        <div class="my-carousel-caption d-none d-md-block">
+          <h1>First slide label</h1>
+          <h3>Nulla vitae elit libero, a pharetra augue mollis interdum.</h3>
+        </div>
       </div>
     </div>
 
   </div>
 
-    <div class="container mt-4">
-      <div class="row">
-        <div class="col-md-12 mb-3">
-          <h1 class="display-4">Circuits les plus populaires</h1>
-        </div>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-12 mb-3">
+        <h1 class="display-4">Circuits les plus populaires</h1>
       </div>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
       </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
     <!-- Optional JavaScript -->
