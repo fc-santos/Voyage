aa79274c39587530ef8075e1a97d7e0876163501
Abdel <abdel.hidalgo@gmail.com>
2019-04-25 12:15:12 -0400

    addcircuit1

---
+++ admin/addcircuit.php
@@ -0,0 +1,27 @@
+<?php include "includes/admin_header.php" ?>
+
+    <div id="wrapper">  
+
+        <!-- Navigation -->
+ 
+        <?php include "includes/admin_navigation.php" ?>   
+
+        <div id="page-wrapper">
+            <ul class="nav nav-tabs">
+                <li role="presentation" class="active"><a href="#">Home</a></li>
+                <li role="presentation"><a href="#">Profile</a></li>
+                <li role="presentation"><a href="#">Messages</a></li>
+            </ul>
+        </div>
+    <!-- /.container-fluid -->
+
+    </div>
+
+     
+        <!-- /#page-wrapper -->
+        
+    <?php include "includes/admin_footer.php" ?>
+
+
+
+
