a0d4dba0fa712620e78155350ae3bdbfc7c18083
Filipe Santos <filipesantos@live.ca>
2019-05-11 16:45:58 -0400

    Création de la page principale de circuits
    Pas encore fini...

---
+++ circuits.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Voyage GoAbroad | Circuits";
+$nav = "circuits";
+include 'includes/header.php';
+
+?>
+<header class="header_circuit">
+    <div class="header_shadow">
+        <div class="">
+            <div class="hero_two header_title text-white shadow p-3 mb-5 rounded bg-gradient-dark">
+                <p>Découvrez Nos</p>
+                <p>Circuits</p>
+            </div>
+        </div>
+    </div>
+</header>
+
+<?php include 'includes/footer.php'; ?>
