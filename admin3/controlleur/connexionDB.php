f8aef8e5b0cfc5ad8e57589b20e3b0c563d460f4
Abdel <abdel.hidalgo@gmail.com>
2019-05-13 16:40:46 -0400

    Update connexionDB.php

--- admin3/controlleur/connexionDB.php
+++ admin3/controlleur/connexionDB.php
@@ -6,6 +6,6 @@
   // Set DSN
   $dsn = 'mysql:host='. $host .';dbname='. $dbname;
   // Create a PDO instance
-  $conn = new PDO($dsn, $user, $password);
+  $conn = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
   $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
