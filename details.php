014f88ddbe16a54e5a930c58fa446f955e418dc1
Filipe Santos <filipesantos@live.ca>
2019-05-16 15:04:00 -0400

    Continuation de la page de détails circuit

--- details.php
+++ details.php
@@ -11,23 +11,79 @@ include 'includes/header.php'; ?>
         <img src="assets/images/village.jpg" class="img-fluid" alt="Image du Circuit">
     </div>
     <div class="mt-4">
-        <h3 class="circuit-description">Description du Circuit</h3>
-        <hr>
-        <p class="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum odio voluptas velit cumque similique. Debitis asperiores possimus temporibus, impedit, eveniet reiciendis quos, sunt autem cumque repellat delectus fugiat magni placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sint, in maiores officia nemo, expedita aperiam, repellendus iusto quos natus necessitatibus cum veniam veritatis harum aliquid dolores accusamus tenetur voluptas. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam esse dolore fugit illum, tempora unde ipsum, itaque provident deserunt ab vero, eveniet ea nulla cupiditate ad optio beatae corporis ullam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam cumque autem laboriosam debitis quia molestiae, iusto quo deleniti ratione quod ea doloremque, ipsum quam vel quis assumenda cum, excepturi cupiditate.</p>
-        <hr>
-        <div class="mt-4">
-            <h3 class="circuit-details mb-4">Détails du Circuit</h3>
-            <div class="jour">
-                <p>Jour 1: Lieu </p>
-                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, quae impedit veritatis cumque voluptates suscipit. Accusamus minima, iure ad et blanditiis ut culpa ullam nihil ea tenetur corrupti earum! Doloribus.</p>
+        <div class="row">
+            <div class="col-md-9">
+                <h3 class="circuit-description">Description du Circuit</h3>
+                <hr>
+                <p class="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repudiandae, inventore illo ratione officiis itaque cum debitis recusandae doloribus aut, sunt, pariatur accusamus nobis mollitia sed. Quod architecto dignissimos illum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolore nisi dicta corporis dolorum, vel nostrum suscipit. Laborum tempora beatae quibusdam incidunt consectetur. Excepturi est adipisci quia. Sequi, aliquam asperiores?</p>
+                <hr>
+                <div class="mt-4">
+                    <h3 class="circuit-details mb-4">Détails du Circuit</h3>
+                    <div id="accordion" role="tablist">
+                        <div class="jour mb-3">
+                            <div class="" role="tab" id="headingOne">
+                                <h5 class="mb-0">
+                                    <a role="button" class="collapsed text-uppercase" data-parent="#accordion" data-toggle="collapse" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                        Jour 1: Lieu <i class="fas fa-angle-down float-right"></i>
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+                                <div class="card-body">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                        <div class="jour mb-3">
+                            <div class="" role="tab" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <a role="button" class="collapsed text-uppercase" data-parent="#accordion" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseOne">
+                                        Jour 2: Lieu<i class="fas fa-angle-down float-right"></i>
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                <div class="card-body">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                        <div class="jour mb-3">
+                            <div class="" role="tab" id="headingThree">
+                                <h5 class="mb-0">
+                                    <a role="button" class="collapsed text-uppercase" data-parent="#accordion" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseOne">
+                                        Jour 3: Lieu<i class="fas fa-angle-down float-right"></i>
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+                                <div class="card-body">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="jour">
-                <p>Jour 2: Lieu </p>
-                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, quae impedit veritatis cumque voluptates suscipit. Accusamus minima, iure ad et blanditiis ut culpa ullam nihil ea tenetur corrupti earum! Doloribus.</p>
-            </div>
-            <div class="jour">
-                <p>Jour 3: Lieu </p>
-                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, quae impedit veritatis cumque voluptates suscipit. Accusamus minima, iure ad et blanditiis ut culpa ullam nihil ea tenetur corrupti earum! Doloribus.</p>
+            <div class="col-md-3">
+                <div class="card mb-5 mb-lg-0 right-side">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted text-uppercase text-center">Réserver Maintenant</h5>
+                        <h6 class="card-price text-center">$4 999</h6>
+                        <hr>
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Phone Support</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+                        </ul>
+                        <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
