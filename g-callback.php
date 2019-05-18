e870b9b71cad8b837e41b15abeb4c1dbef049adf
Abdel <abdel.hidalgo@gmail.com>
2019-05-18 17:32:40 -0400

    fixed registration from google

--- g-callback.php
+++ g-callback.php
@@ -4,9 +4,9 @@ require_once 'controlleur/connexionDB.php';
 
 require_once 'google.php';
 
-if (isset($_SESSION['access_token']))
+if (isset($_SESSION['access_token'])) {
     $gClient->setAccessToken($_SESSION['access_token']);
-else if (isset($_GET['code'])) {
+} elseif (isset($_GET['code'])) {
     $token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
     $_SESSION['access_token'] = $token;
 } else {
@@ -20,11 +20,35 @@ $userData = $oAuth->userinfo_v2_me->get();
 $email = $userData['email'];
 $prenom = $userData['familyName'];
 $nom = $userData['givenName'];
+$password = "1234";
+
+header("location: index.php");
 //$_SESSION['picture'] = $userData['picture'];
 
-$stmt = $conn->prepare('SELECT prenom, nom, courriel, role FROM utilisateur WHERE courriel = ?');
+/*$stmt = $conn->prepare('SELECT prenom, nom, courriel, role FROM utilisateur WHERE courriel = ?');
 $stmt->execute([$email]);
-$user = $stmt->fetch();
+$user = $stmt->fetch();*/
+
+$query = "SELECT prenom, nom, courriel, role FROM utilisateur WHERE courriel = :email";
+$stmt1 = $conn->prepare($query);
+$stmt1->execute(['courriel' => $email]);
+
+if ($stmt1->rowCount() == 0) {
+    $query1 = "INSERT INTO `utilisateur`(`prenom`, `nom`, `courriel`, `password`, `sexe`, `adresse`, `ville`, `codePostal`, `pays`, `role`) VALUES (:prenom, :nom, :courriel, '$password', null, null, null, null, null, 'Membre')";
+    $stmt2 = $conn->prepare($query1);
+    $stmt2->execute(['prenom' => $prenom, 'nom' => $nom, 'courriel' => $email]);
+    $_SESSION['prenom'] = $prenom;
+    $_SESSION['nom'] = $nom;
+    $_SESSION['courriel'] = $courriel;
+    $_SESSION['role'] = 'Membre';
+} else {
+    $_SESSION['prenom'] = $user->prenom;
+    $_SESSION['nom'] = $user->nom;
+    $_SESSION['courriel'] = $user->courriel;
+    $_SESSION['role'] = $user->role;
+}
+
+/*
 if ($user) {
     $_SESSION['prenom'] = $user->prenom;
     $_SESSION['nom'] = $user->nom;
@@ -38,7 +62,7 @@ if ($user) {
     $_SESSION['nom'] = $nom;
     $_SESSION['courriel'] = $courriel;
     $_SESSION['role'] = 'Membre';
-}
+}*/
 
 header('Location: index.php');
 exit();
