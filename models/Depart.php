265a30d1e2c972fd95eee3456e7b485cb75cda67
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-06-05 15:38:57 -0400

    Add API to List All Departs

---
+++ models/Depart.php
@@ -0,0 +1,50 @@
+<?php
+
+    class Depart {
+
+        // DB Properties
+        private $conn;
+        
+        // Départ Properties
+        public $idDepart;
+        public $idCircuit;
+        public $circuitName;
+        public $dateDebut;
+        public $nbPlaces;
+        public $prix;
+        public $titrePromotion;
+        public $rabais;
+        public $estActif;
+        
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        // GET Departs
+        public function listeDeparts() {
+            
+            // Create query
+            $query = 'SELECT 
+                    c.titre as circuitName,
+                    d.idDepart,
+                    d.idCircuit,
+                    d.dateDebut,
+                    d.nbPlaces,
+                    d.prix,
+                    d.titrePromotion,
+                    d.rabais,
+                    d.estActif 
+                FROM depart d
+                LEFT JOIN
+                    circuit c ON d.idCircuit = c.idCircuit';
+
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            // Execute statement
+            $stmt->execute();
+
+            return $stmt;
+        }
+    }
+    
